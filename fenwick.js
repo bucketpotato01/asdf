d8c40a65236aa748f816539d9c17c0f243d42a2d
bucketpotato <bucketpotato@gmail.com>
2021-08-21 23:19:52 -0700

    changed fenwick page to look like segtree page

--- fenwick.js
+++ fenwick.js
@@ -5,6 +5,7 @@ var nodeBorderWidth = 1;   // name is self-explanatory
 var nodeSize = 8;         // radius of the nodes on the svg
 var markColor = '#ffd7d4'  // the color for marking nodes
 var markWidth = 2;         // radius of marked nodes' borders
+var queryMark = '#ffa6a6';
 
 var textSize = 10               // size of the text in the nodes
 var textOffset = textSize / 3;  // make the nodes pretty
@@ -16,6 +17,7 @@ const edgeIDPrefix = "stEdge";     // prefix for ID of an edge
 const outlineIDPrefix = "outline"; // prefix for ID of a outline
 const bitVisID = "bitvis";          // ID of the svg
 const valInputID = "valueInput";   // ID of the value input
+const indInputIDQ = "indexInputq";
 const indInputID = "indexInput";   // ID of the index input
 const lInputID = "linput";
 const rInputID = "rinput";
@@ -35,163 +37,6 @@ var popupFill = '#e6e6e6';
 var popupBorder = '#000000';
 const popupBorderWidth = 1;
 
-const aboutText = `You are given an array, and you should be able to modify elements and find the sum of a range in the array. Fenwick trees, or Binary Indexed trees (BIT) accomplish this in logarithmic time per operation.
-Update operations: there is at most one node of each size that covers any one index. Iterating through and changing these nodes takes lg(N) time.
-Query operations: Traversing the tree rightwards until the zero node will go through at most lg(N) nodes.`;
-
-const bitotheruses = `Each index is assigned a range to cover based on its lowest set bit: for example, the lowest set bit of 22 (10110 in binary) would be 2 (00010 in binary), so index 22 would cover a range of size 2.`;
-
-function addlabel(svgid, x, y, whatToWrite, nodeInd) {
-
-	if (document.getElementById(infoIDPrefix + svgid) != null) {
-		var cind = document.getElementById(infoIDPrefix + svgid).getAttribute('ind');
-		document.getElementById(infoIDPrefix + svgid).remove();
-		if (cind == nodeInd) return;
-	} 
-
-	var popup = document.createElement('g');
-	popup.setAttribute('id', infoIDPrefix + svgid);
-	popup.setAttribute('ind', nodeInd);
-
-	var curroffset = 0;
-
-	if (x + popupWidth > document.getElementById(svgid).width.animVal.value)
-		curroffset = -popupWidth;
-	
-	var bg = document.createElement('rect');
-	bg.setAttribute('x', x + curroffset);
-	bg.setAttribute('y', y - popupHeight);
-	bg.setAttribute('width', popupWidth);
-	bg.setAttribute('height', popupHeight);
-	bg.setAttribute('align', 'bottom');
-
-	bg.setAttribute('fill', popupFill);
-	bg.setAttribute('stroke', popupBorder);
-	bg.setAttribute('stroke-width', popupBorderWidth);
-
-	popup.appendChild(bg);
-
-	var s = document.createElement('text');
-
-	s.setAttribute('x', x + popupWidth / 2 + curroffset);
-	s.setAttribute('y', y - popupHeight / 2.5);
-	s.setAttribute('text-anchor', 'middle');
-	s.setAttribute('font-size', popupTextSize);
-
-	s.innerHTML = whatToWrite;
-
-	popup.appendChild(s);
-
-	document.getElementById(svgid).appendChild(popup);
-
-	document.getElementById(svgid).innerHTML += '';
-
-}
-
-class AboutFenwick {
-
-	constructor(ts) {
-		this.ts = ts;
-
-		this.addAbout();
-		this.addAboutUpdate();
-		this.addAboutQuery();
-
-		ts.innerHTML += "";
-
-	}
-
-	addDivider() {
-		var ts = this.ts;
-		var chr = document.createElement("hr");
-		ts.appendChild(chr);
-	}
-
-	addText(where, what) {
-
-		var newp = document.createElement("p");
-		newp.innerHTML = what;
-		document.getElementById(where).appendChild(newp);
-
-	}
-
-	makeSpoiler(sptxt, tospoiler) {
-		var res = document.createElement("b");
-		res.innerHTML = sptxt;
-
-		var v1 = "";
-		var v2 = "";
-		for (var j = 0; j < tospoiler.length; j++) {
-			var i = tospoiler[j];
-			v1 = v1 + `document.getElementById('${i}').style.display = '';`;
-			v2 = v2 + `document.getElementById('${i}').style.display = 'none';`;
-		}
-		var finif = `if (document.getElementById('${tospoiler[0]}').style.display == 'none') {${v1}} else {${v2}}`;
-
-		res.setAttribute("onclick", finif);
-		var fr = document.createElement("p");
-		fr.appendChild(res);
-		return fr;
-
-	}
-
-	hiddenText(ttxt, tid) {
-		var res = document.createElement("p");
-		res.setAttribute("id", tid);
-		res.setAttribute("style", "display: none;");
-		res.innerHTML = ttxt;
-		return res;
-	}
-
-	addAbout() {
-
-		var ts = this.ts;
-		var texttowrite = aboutText.split("\n");
-
-		this.addText(whereToWrite, texttowrite[0]);
-
-		var bt = this.makeSpoiler("How does the BIT do this?", ["moreaboutbitusage"]);
-		var moreinfo = this.hiddenText(bitotheruses, "moreaboutbitusage");
-		
-		ts.appendChild(bt);
-		ts.appendChild(moreinfo);
-
-
-		this.addDivider();
-
-	}
-
-	addAboutUpdate() {
-
-		var ts = this.ts;
-		var texttowrite = aboutText.split("\n");
-
-		this.addText(whereToWrite, texttowrite[1]);
-		ts.innerHTML += "<div class='maindiv'><svg id=uexsvg width='512' height='512'></svg></div>";
-		var uexst = new Fenwick("uexsvg", 63);
-		uexst.upd(17, 1);
-
-
-		this.addDivider();
-
-	}
-
-	addAboutQuery() {
-
-		var ts = this.ts;
-		var texttowrite = aboutText.split("\n");
-
-		this.addText(whereToWrite, texttowrite[2]);
-		ts.innerHTML += "<div class='maindiv'><svg id=qexsvg width='512' height='512'></svg></div>";
-		var qexst = new Fenwick("qexsvg", 63);
-		qexst.query(55);
-
-
-		this.addDivider();
-
-	}
-
-}
 
 class Fenwick {
 
@@ -270,13 +115,11 @@ class Fenwick {
 		var infoText = "Covers [" + (currID - (currID & (-currID)) + 1) + ", " + currID + "]";
 		infoText = this.toBinaryString(currID) + ": " + infoText;
 
-		// console.log(infoText);
-
 		if (currID == 0)
 			infoText = "Not actually used";
 
 		displayNode.setAttribute('onclick', 
-			'addlabel(' + 
+			'Fenwick.addlabel(' + 
 			('"' + this.id + '"') + ',' 
 			+ x + ',' 
 			+ y + ',' 
@@ -417,58 +260,96 @@ class Fenwick {
 		this.vis().innerHTML = "";
 		this.draw();
 	}
+
+	static addlabel(svgid, x, y, whatToWrite, nodeInd) {
+
+		if (document.getElementById(infoIDPrefix + svgid) != null) {
+			var cind = document.getElementById(infoIDPrefix + svgid).getAttribute('ind');
+			document.getElementById(infoIDPrefix + svgid).remove();
+			if (cind == nodeInd) return;
+		}
+
+		var popup = document.createElement('g');
+		popup.setAttribute('id', infoIDPrefix + svgid);
+		popup.setAttribute('ind', nodeInd);
+
+		var curroffset = 0;
+		if (x + popupWidth > document.getElementById(svgid).width.animVal.value)
+			curroffset = -popupWidth;
+		
+		var bg = document.createElement('rect');
+		bg.setAttribute('x', x + curroffset);
+		bg.setAttribute('y', y - popupHeight);
+		bg.setAttribute('width', popupWidth);
+		bg.setAttribute('height', popupHeight);
+		bg.setAttribute('align', 'bottom');
+
+		bg.setAttribute('fill', popupFill);
+		bg.setAttribute('stroke', popupBorder);
+		bg.setAttribute('stroke-width', popupBorderWidth);
+
+		popup.appendChild(bg);
+
+		var s = document.createElement('text');
+
+		s.setAttribute('x', x + popupWidth / 2 + curroffset);
+		s.setAttribute('y', y - popupHeight / 2.5);
+		s.setAttribute('text-anchor', 'middle');
+		s.setAttribute('font-size', popupTextSize);
+
+		s.innerHTML = whatToWrite;
+
+		popup.appendChild(s);
+
+		document.getElementById(svgid).appendChild(popup);
+
+		document.getElementById(svgid).innerHTML += '';
+
+	}
+
 }
 
 
 class UserInteraction {
 
-	constructor(ms) {
-		this.bit = ms;
-	}
+	static fenwick;
 
-	tellUser(message) {
+	static tellUser(message) {
 		document.getElementById(whereToWrite).innerHTML = message;
 	}
 
-	updateOperation() {
+	static updateOperation() {
 		
-		this.tellUser("");
+		UserInteraction.tellUser("");
 		var val = parseInt(document.getElementById(valInputID).value);
-		var ind = parseInt(document.getElementById(indInputID).value);
+		var ind = parseInt(document.getElementById(indInputIDQ).value);
 		
 		if (isNaN(val) || isNaN(ind)) return;
-		if (ind > this.bit.arrSize || ind <= 0) {
-			this.tellUser("Index out of bounds");
+		if (ind >= UserInteraction.fenwick.arrSize || ind < 0) {
+			UserInteraction.tellUser("Index out of bounds");
 			return;
 		}
-		this.bit.upd(ind, val);
+		UserInteraction.fenwick.upd(ind, val);
 
 	}
 
-	queryOperation() {
+	static queryOperation() {
 
 		var l = parseInt(document.getElementById(indInputID).value);
 		
 		if (isNaN(l)) return;
-		if (l > this.bit.arrSize || l <= 0) {
-			this.tellUser("Index out of bounds");
+		if (l > UserInteraction.fenwick.arrSize || l <= 0) {
+			UserInteraction.tellUser("Index out of bounds");
 			return;
 		}
 
-		var res = this.bit.query(l);
+		var res = UserInteraction.fenwick.query(l);
 
-		this.tellUser("Result of query is: " + res);
+		UserInteraction.tellUser("Result of query is: " + res);
 
 	}
 
-	resetAll() {
-		var cp = document.getElementById(controlID);
-		cp.innerHTML = "";
-		var txt = document.getElementById(whereToWrite);
-		txt.innerHTML = "";
-	}
-
-	addText(where, what) {
+	static addText(where, what) {
 
 		var newp = document.createElement("p");
 		newp.innerHTML = what;
@@ -476,202 +357,114 @@ class UserInteraction {
 
 	}
 
-	setHTMLToAbout() {
-		
-		this.resetAll();
-		document.getElementById(controlID).innerHTML = "";
-
-		var ts = document.getElementById(whereToWrite);
-
-		var helper = new AboutFenwick(ts);
-
-	}
-
-	setHTMLToUpdate() {
-		
-		this.resetAll();
-		var cp = document.getElementById(controlID);
-
-		var cdiv = document.createElement("div");
-
-		var valv = document.createElement("input");
-		valv.setAttribute("type", "number");
-		valv.setAttribute("id", valInputID);
-		valv.setAttribute("placeholder", "Value to add");
-
-		cdiv.appendChild(valv);
-
-		var indv = document.createElement("input");
-		indv.setAttribute("type", "number");
-		indv.setAttribute("id", indInputID);
-		indv.setAttribute("placeholder", "Index to add to");
-
-		cdiv.appendChild(indv);
-
-		var button = document.createElement("button");
-		button.setAttribute("onclick", "interaction.updateOperation()");
-		button.innerHTML = "Go";
-
-		cdiv.appendChild(button);
-
-		cp.appendChild(cdiv);
-
-	}
-
-	setHTMLToQuery() {
-		
-		this.resetAll();
-		var cp = document.getElementById(controlID);
-
-		var cdiv = document.createElement("div");
-
-		var valv = document.createElement("input");
-		valv.setAttribute("type", "number");
-		valv.setAttribute("id", indInputID);
-		valv.setAttribute("placeholder", "Upper bound of query");
-
-		cdiv.appendChild(valv);
-
-		var button = document.createElement("button");
-		button.setAttribute("onclick", "interaction.queryOperation()");
-		button.innerHTML = "Go";
-
-		cdiv.appendChild(button);
-
-		cp.appendChild(cdiv);
-
-	}
-
-	checkIfValid(v, ov) {
+	static checkIfValid(v, ov) {
 		if (isNaN(parseInt(v))) return ov;
 		return parseInt(v);
 	}
 
-	setoptions() {
+	static updopsteps(numSteps, indToUpd, valToAdd) {
+		this.uexst = new Fenwick("uexsvg", 31)
+		this.uexst.unmarkall();
+		for (var i = 0; i < this.uexst.stsize; i++)
+			this.uexst.set(i, 0);
 
-		var coloroptions = [
-			["Node Fill", nodeColor],
-			["Node Border", nodeBorder],
-			["Mark Color 1", markColor],
-			["Popup Fill", popupFill],
-			["Popup Border", popupBorder]
-		]
-			
-		nodeColor = document.getElementById(coloroptions[0][0]).value;
-		nodeBorder = document.getElementById(coloroptions[1][0]).value;
-		markColor = document.getElementById(coloroptions[2][0]).value;
-		popupFill = document.getElementById(coloroptions[3][0]).value;
-		popupBorder = document.getElementById(coloroptions[4][0]).value;
+		var pv = -1;
 
-		var cosmeticOptions = [
-			 ["Node Size", nodeSize]
-			,["Node Border Width", nodeBorderWidth]
-			,["Popup Width", popupWidth]
-			,["Popup Height", popupHeight]
-			,["Text Size", textSize]
-		];
+		for (var i = 0; i < numSteps; i++) {
+			if (indToUpd < this.uexst.arrSize) {
+				this.uexst.set(indToUpd, valToAdd);
+				this.uexst.mark(indToUpd, true, queryMark);
+			}
+			pv = indToUpd;
+			indToUpd = (indToUpd) + (indToUpd & (-indToUpd));
+		}
 
-		nodeSize = this.checkIfValid(document.getElementById(cosmeticOptions[0][0]).value, nodeSize);
-		nodeBorderWidth = this.checkIfValid(document.getElementById(cosmeticOptions[1][0]).value, nodeBorderWidth);
-		popupWidth = this.checkIfValid(document.getElementById(cosmeticOptions[2][0]).value, popupWidth);
-		popupHeight = this.checkIfValid(document.getElementById(cosmeticOptions[3][0]).value, popupHeight);
-		textSize = this.checkIfValid(document.getElementById(cosmeticOptions[4][0]).value, textSize);
-		textOffset = textSize / 3;
-		
-		this.bit.redraw();
+		if (indToUpd >= this.uexst.arrSize) {
+			document.getElementById('updinfotxt').innerHTML = `Now on node with index ${indToUpd}, finished.`;
+		}
+		else {
+			this.uexst.mark(indToUpd);
+			document.getElementById('updinfotxt').innerHTML = `Just updated node with index ${pv}, now at node with index (${pv} + ${(pv & (-pv))}) = ${indToUpd}.`;
+		}
+
 
 	}
 
-	setHTMLToSettings() {
+}
 
-		this.resetAll();
+class Resizer {
 
-		var cp = document.getElementById(controlID);
-		// cp.setAttribute("class", "maindiv");
+	// https://htmldom.dev/create-resizable-split-views/
 
-		var dcolors = document.createElement("div");
-		dcolors.setAttribute("class", "uidiv");
+	static x;
+	static y;
+	static lwidth;
+	static fv;
 
-		var coloroptions = [
-			 ["Node Fill", nodeColor]
-			,["Node Border", nodeBorder]
-			,["Mark Color 1", markColor]
-			,["Popup Fill", popupFill]
-			,["Popup Border", popupBorder]
-		]
+	static c;
+	static l;
+	static r;
 
-		for (var i = 0; i < coloroptions.length; i++) {
-			var tdiv = document.createElement("div");
-			tdiv.setAttribute("class", "regdiv");
-			var wone = document.createElement("label");
-			wone.innerHTML = coloroptions[i][0];
-			var cin = document.createElement("input");
-			cin.setAttribute("type", "color");
-			cin.setAttribute("value", coloroptions[i][1]);
-			cin.setAttribute("placeholder", coloroptions[i][0]);
-			cin.setAttribute("id", coloroptions[i][0]);
-			cin.setAttribute("class", "smallinput");
-			tdiv.appendChild(wone);
-			tdiv.appendChild(cin);
-			dcolors.appendChild(tdiv);
-		}
+	static resize(e) {
 
-		var dcosmetic = document.createElement("div");
-		dcosmetic.setAttribute("class", "uidiv");
+		var dx = e.clientX - Resizer.x;
+		var dy = e.clientY - Resizer.y;
 
-		var cosmeticOptions = [
-			 ["Node Size", nodeSize]
-			,["Node Border Width", nodeBorderWidth]
-			,["Popup Width", popupWidth]
-			,["Popup Height", popupHeight]
-			,["Text Size", textSize]
-		];
+		var nlw = (Resizer.lwidth + dx) * 100 / Resizer.c.parentNode.getBoundingClientRect().width;
+		Resizer.l.style.width = `${nlw}%`;
 
-		for (var i = 0; i < cosmeticOptions.length; i++) {
-			var tdiv = document.createElement("div");
-			tdiv.setAttribute("class", "regdiv");
-			var wone = document.createElement("label");
-			wone.innerHTML = cosmeticOptions[i][0];
-			var cin = document.createElement("input");
-			cin.setAttribute("type", "number");
-			cin.setAttribute("value", cosmeticOptions[i][1]);
-			cin.setAttribute("placeholder", cosmeticOptions[i][0]);
-			cin.setAttribute("id", cosmeticOptions[i][0]);
-			cin.setAttribute("class", "smallinput");
-			tdiv.appendChild(wone);
-			tdiv.appendChild(cin);
-			dcosmetic.appendChild(tdiv);
-		}
-
-		var odiv = document.createElement("div");
-
-		var butt = document.createElement("button");
-		butt.innerHTML = "Save";
-		butt.setAttribute("onclick", "interaction.setoptions()");
-		odiv.appendChild(butt);
-
-		cp.appendChild(dcolors);
-		cp.appendChild(dcosmetic);
-		cp.appendChild(odiv);
-
-		cp.innerHTML += "";
 	}
 
+	static stopresize(e) {
+
+		Resizer.l.style.removeProperty('user-select');
+		Resizer.l.style.removeProperty('pointer-events');
+		Resizer.r.style.removeProperty('user-select');
+		Resizer.r.style.removeProperty('pointer-events');
+
+		document.removeEventListener('mousemove', Resizer.resize);
+		document.removeEventListener('mouseup', Resizer.stopresize);
+	}
+
+	static sclick(e) {
+
+		Resizer.x = e.clientX;
+		Resizer.y = e.clientY;
+		Resizer.lwidth = Resizer.l.getBoundingClientRect().width;
+
+		Resizer.l.style.userSelect = 'none';
+		Resizer.l.style.pointerEvents = 'none';
+		Resizer.r.style.userSelect = 'none';
+		Resizer.r.style.pointerEvents = 'none';
+	
+
+		document.addEventListener('mouseup', Resizer.stopresize);
+		document.addEventListener('mousemove', Resizer.resize);
+
+	}
+
+	static sresize() {
+
+		Resizer.c = document.getElementById('resizer');
+		Resizer.l = Resizer.c.previousElementSibling;
+		Resizer.r = Resizer.c.nextElementSibling;
+
+		Resizer.lwidth = Resizer.l.getBoundingClientRect().width;
+		document.getElementById('resizer').addEventListener("mousedown", Resizer.sclick);
+	}
 }
 
 var interaction;
-
-function switchStuff() {
-	var chosen = document.getElementById(selectionBar).value;
-	if (chosen == updateOption) interaction.setHTMLToUpdate();
-	if (chosen == aboutOption) interaction.setHTMLToAbout();
-	if (chosen == queryOption) interaction.setHTMLToQuery();
-	if (chosen == settingsOption) interaction.setHTMLToSettings();
-}
+var helpabout;
 
 function initall() {
 
-	interaction = new UserInteraction( new Fenwick(bitVisID) );
-	switchStuff();
+	UserInteraction.fenwick = new Fenwick(bitVisID);
+
+	Resizer.sresize();
+
+	var uexst = new Fenwick("uexsvg", 31);
+	var qexst = new Fenwick("qexsvg", 15);
+	qexst.query(7);
 
 }
